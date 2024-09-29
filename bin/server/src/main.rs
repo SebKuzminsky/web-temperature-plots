65e3853193b2d32d306228077358629f53dfdf2b
Sebastian Kuzminsky <seb@highlab.com>
2024-09-29 13:27:25 -0600

    start adding a server process to push data to the yew ui

---
+++ bin/server/src/main.rs
@@ -0,0 +1,11 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let s = yew_hello_world::Stats {
+        temperatures: [ 0.1, 1.0, 2.0, 3.0, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10 ],
+    };
+    println!("s = {:?}", s);
+
+    let js = serde_json::to_string(&s).unwrap();
+    println!("js = {}", js);
+
+    Ok(())
+}
