395ecf67c5642c956aed7f3f22de5e4939b717fa
Sebastian Kuzminsky <seb@highlab.com>
2024-09-29 20:48:25 -0600

    lib: add Stats::new()

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,23 @@ use serde;
 pub struct Stats {
     pub temperatures: [f32; 11],
 }
+
+impl Stats {
+    pub fn new() -> Self {
+        Self {
+            temperatures: [
+                -999_f32,
+                -999_f32,
+                -999_f32,
+                -999_f32,
+                -999_f32,
+                -999_f32,
+                -999_f32,
+                -999_f32,
+                -999_f32,
+                -999_f32,
+                -999_f32,
+            ],
+        }
+    }
+}
