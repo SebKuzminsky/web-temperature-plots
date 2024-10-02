a86b40413af5a24d3f179c0a53aa5cc4d454725b
Sebastian Kuzminsky <seb@scytherobotics.com>
2024-10-02 10:13:35 -0600

    server: simplify polling for stats

--- host-side/src/bin/server/main.rs
+++ host-side/src/bin/server/main.rs
@@ -45,13 +45,14 @@ async fn handle_tcp_client_inner(
 async fn poll_stats(
     stats: std::sync::Arc<tokio::sync::Mutex<yew_hello_world::Stats>>
 ) {
+    println!("polling for stats");
     loop {
-        {
-            let mut locked_stats = stats.lock().await;
-            locked_stats.temperatures[0] += 0.0;
-            locked_stats.temperatures[1] += 0.1;
-            locked_stats.temperatures[2] += 0.2;
-        }
+        let mut locked_stats = stats.lock().await;
+        locked_stats.temperatures[0] += 0.0;
+        locked_stats.temperatures[1] += 0.1;
+        locked_stats.temperatures[2] += 0.2;
+        drop(locked_stats);
+
         tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
     }
 }
