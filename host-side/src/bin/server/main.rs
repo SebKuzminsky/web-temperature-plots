a06ab1e01621fe3f067cf8ee2f1607b45740a3dd
Sebastian Kuzminsky <seb@scytherobotics.com>
2024-10-02 11:19:46 -0600

    server: add a websockets server

--- host-side/src/bin/server/main.rs
+++ host-side/src/bin/server/main.rs
@@ -1,6 +1,17 @@
 use futures::prelude::*;
 
 
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    #[error("websockets error")]
+    TWSError(#[from] tokio_websockets::Error),
+    #[error("std::io error")]
+    StdIoError(#[from] std::io::Error),
+    #[error("serde_json error")]
+    SerdeJsonError(#[from] serde_json::Error),
+}
+
+
 async fn handle_tcp_client(
     socket: tokio::net::TcpStream,
     stats: std::sync::Arc<tokio::sync::Mutex<yew_hello_world::Stats>>
@@ -36,7 +47,35 @@ async fn handle_tcp_client_inner(
         let locked_stats = stats.lock().await;
         println!("{:#?}", *locked_stats);
         serialized.send(*locked_stats).await?;
+        tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
+    }
+}
 
+
+async fn handle_ws_client(
+    stream: tokio_websockets::WebSocketStream<tokio::net::TcpStream>,
+    stats: std::sync::Arc<tokio::sync::Mutex<yew_hello_world::Stats>>
+) {
+    let client_id = format!("{:?}", &stream);
+    match handle_ws_client_inner(stream, stats).await {
+        Ok(()) => println!("done with ws client {:?}: Ok!", client_id),
+        Err(e) => println!("error with ws client {:?}: {:#?}", client_id, e),
+    }
+}
+
+
+async fn handle_ws_client_inner(
+    mut stream: tokio_websockets::WebSocketStream<tokio::net::TcpStream>,
+    stats: std::sync::Arc<tokio::sync::Mutex<yew_hello_world::Stats>>
+) -> Result<(), Error> {
+    let client_id = format!("{:?}", &stream);
+    loop {
+        println!("sending to ws client {client_id}");
+        let locked_stats = stats.lock().await;
+        let json = serde_json::to_string(&*locked_stats)?;
+        drop(locked_stats);
+        println!("{}", json);
+        stream.send(tokio_websockets::Message::text(json)).await?;
         tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
     }
 }
@@ -71,6 +110,34 @@ async fn tcp_server(
 }
 
 
+async fn ws_server(
+    stats: std::sync::Arc<tokio::sync::Mutex<yew_hello_world::Stats>>,
+) {
+    loop {
+        match ws_server_inner(std::sync::Arc::clone(&stats)).await {
+            Ok(()) => println!("ws_server returned ok??"),
+            Err(e) =>  println!("ws_server returned error: {:#?}", e),
+        };
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    }
+}
+
+
+async fn ws_server_inner(
+    stats: std::sync::Arc<tokio::sync::Mutex<yew_hello_world::Stats>>,
+) -> Result<(), Error> {
+    let tcp_listener = tokio::net::TcpListener::bind("localhost:7655").await?;
+    println!("listening for WS connections {:#?}", tcp_listener);
+    loop {
+        let (tcp_stream, _) = tcp_listener.accept().await?;
+        println!("ws_server accepted TCP connection");
+        let ws_stream = tokio_websockets::ServerBuilder::new().accept(tcp_stream).await?;
+        println!("accepted ws: {:#?}", ws_stream);
+        tokio::spawn(handle_ws_client(ws_stream, std::sync::Arc::clone(&stats)));
+    }
+}
+
+
 #[tokio::main]
 async fn main() {
     let stats = std::sync::Arc::new(tokio::sync::Mutex::new(yew_hello_world::Stats::new()));
@@ -86,8 +153,9 @@ async fn main() {
         println!("back to s: {:?}", new_stats);
     }
 
-    let (_, _) = tokio::join!(
+    let (_, _, _) = tokio::join!(
         tokio::spawn(poll_stats(std::sync::Arc::clone(&stats))),
         tokio::spawn(tcp_server(std::sync::Arc::clone(&stats))),
+        tokio::spawn(ws_server(std::sync::Arc::clone(&stats))),
     );
 }
