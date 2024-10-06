402e1cc4f0bcf7e9f76e451c6254ae4f569c27c6
Sebastian Kuzminsky <seb@highlab.com>
2024-10-05 21:59:43 -0600

    server: move stats out to its own module

--- host-side/src/bin/server/main.rs
+++ host-side/src/bin/server/main.rs
@@ -1,22 +1,7 @@
 mod websockets;
 mod tcp;
+mod stats;
 mod error;
-
-
-async fn poll_stats(
-    stats: std::sync::Arc<tokio::sync::Mutex<yew_hello_world::Stats>>
-) {
-    println!("polling for stats");
-    loop {
-        let mut locked_stats = stats.lock().await;
-        locked_stats.temperatures[0] += 0.0;
-        locked_stats.temperatures[1] += 0.1;
-        locked_stats.temperatures[2] += 0.2;
-        drop(locked_stats);
-
-        tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
-    }
-}
 
 
 #[tokio::main]
@@ -24,7 +9,7 @@ async fn main() {
     let stats = std::sync::Arc::new(tokio::sync::Mutex::new(yew_hello_world::Stats::new()));
 
     let (_, _, _) = tokio::join!(
-        tokio::spawn(poll_stats(std::sync::Arc::clone(&stats))),
+        tokio::spawn(stats::poll_stats(std::sync::Arc::clone(&stats))),
         tokio::spawn(tcp::tcp_server(std::sync::Arc::clone(&stats))),
         tokio::spawn(websockets::ws_server(std::sync::Arc::clone(&stats))),
     );
