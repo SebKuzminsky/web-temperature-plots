f2c7bacd5343484b8cf66f866f812a72f67e12ad
Sebastian Kuzminsky <seb@highlab.com>
2024-10-05 22:35:40 -0600

    server: get real temps for the stats :-)

--- host-side/src/bin/server/stats.rs
+++ host-side/src/bin/server/stats.rs
@@ -2,12 +2,21 @@ pub async fn poll_stats(
     stats: std::sync::Arc<tokio::sync::Mutex<yew_hello_world::Stats>>
 ) {
     println!("polling for stats");
+
     loop {
         let mut locked_stats = stats.lock().await;
-        locked_stats.temperatures[0] += 0.0;
-        locked_stats.temperatures[1] += 0.1;
-        locked_stats.temperatures[2] += 0.2;
+
+        for i in 0..=10 {
+            let filename = format!("/sys/devices/virtual/thermal/thermal_zone{i}/temp");
+            let contents = std::fs::read_to_string(&filename)
+                .unwrap_or_else(|e|{todo!("failed to read file '{filename}': {e:?}")});
+            let contents = contents.trim();
+            let t = contents.parse::<f32>().unwrap_or_else(|e| { todo!("failed to parse f32 from '{contents}': {e:?}") }) / 1_000.0;
+            locked_stats.temperatures[i] = t;
+        }
+
         drop(locked_stats);
+
         tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
     }
 }
