217b10e5d2ca165c4e63603d255b97b78fba0d8b
Sebastian Kuzminsky <seb@highlab.com>
2024-10-05 21:45:59 -0600

    plot a temperature from stats!

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ async fn get_stats_inner(stats_cb: &Callback<stats::Stats>) -> Result<(), anyhow
 
 #[derive(Clone, PartialEq)]
 pub struct App {
-    stats: Option<stats::Stats>,
+    stats: Vec<stats::Stats>,
     canvas: NodeRef,
 }
 
@@ -66,11 +66,9 @@ impl Component for App {
         let stats_cb = ctx.link().callback(Msg::Stats);
         spawn_local(make_up_stats(stats_cb));
 
-        ctx.link().send_message(Msg::Redraw);
-
         log!("App::create() is done");
         Self {
-            stats: None,
+            stats: vec![],
             canvas: NodeRef::default(),
         }
     }
@@ -80,31 +78,47 @@ impl Component for App {
         match msg {
             Msg::Stats(s) => {
                 // log!(format!("got stats: {:#?}", s));
-                self.stats = Some(s);
+                self.stats.push(s);
                 ctx.link().send_message(Msg::Redraw);
             },
 
             Msg::Redraw => {
+                // massage data into the format plotters wants
+                let mut data: Vec<(f32, f32)> = vec![];
+                for (i, &s) in self.stats.iter().enumerate() {
+                    data.push((i as f32, s.temperatures[1]));
+                }
+                log!(format!("{:?}", data));
+                let x_max = data.len() - 1;
+                let y_max = self.stats.iter().map(|s| s.temperatures[1]).reduce(f32::max).unwrap();
+                let y_min = self.stats.iter().map(|s| s.temperatures[1]).reduce(f32::min).unwrap();
+                let line_series = LineSeries::new(data, &RED);
+
                 let element: HtmlCanvasElement = self.canvas.cast().unwrap();
                 let rect = element.get_bounding_client_rect();
                 log!(format!("bounding rectangle width={} height={}", rect.width(), rect.height()));
-                element.set_height(rect.height() as u32);
-                element.set_width(rect.width() as u32);
+                element.set_width(640);
+                element.set_height(480);
+
                 let backend = CanvasBackend::with_canvas_object(element).unwrap();
                 let drawing_area = backend.into_drawing_area();
-                drawing_area.fill(&RGBColor(200,200,200)).unwrap();
+                drawing_area.fill(&RGBColor(240,240,240)).unwrap();
+
                 let mut chart = ChartBuilder::on(&drawing_area)
-                    .caption("y=x^2", ("sans-serif", 14).into_font())
+                    .caption("Temperature", ("sans-serif", 14).into_font())
                     .margin(5)
-                    .x_label_area_size(30)
-                    .y_label_area_size(30)
-                    .build_cartesian_2d(-1_f32..1_f32, -0.1_f32..1_f32).unwrap();
-                chart.configure_mesh().draw().unwrap();
+                    .x_label_area_size(25)
+                    .y_label_area_size(50)
+                    .build_cartesian_2d(0_f32..(x_max as f32), y_min..y_max).unwrap();
+
+                chart.configure_mesh()
+                    .x_label_formatter(&|x| format!("{}", x.round()))
+                    .y_label_formatter(&|y| format!("{y:.2}"))
+                    .draw()
+                    .unwrap();
+
                 chart
-                    .draw_series(LineSeries::new(
-                        (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x*x)),
-                        &RED,
-                    )).unwrap()
+                    .draw_series(line_series).unwrap()
                     .label("y = x^2")
                     .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
                 return false;
@@ -129,15 +143,16 @@ impl Component for App {
         // let vnode = VNode::VRef(node);
         // vnode.into()
 
+        let stats_html = match self.stats.last() {
+            None => html!{<p>{"Waiting for stats..."}</p>},
+            Some(s) => html!{<p>{format!("{:#?}", s)}</p>},
+        };
+
         html! {
             <>
                 <counter::Counter/>
                 <hr/>
-                if let Some(stats) = self.stats {
-                    <p>{format!("{:#?}", stats)}</p>
-                } else {
-                    <p>{"Waiting for stats..."}</p>
-                }
+                {stats_html}
                 <hr/>
                 <canvas ref={self.canvas.clone()}/>
             </>
