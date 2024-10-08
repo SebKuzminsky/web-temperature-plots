4d837b9b2d6c899f83db92cb9ceb8200aea5cdd4
Sebastian Kuzminsky <seb@highlab.com>
2024-10-07 21:53:02 -0600

    simplify stats

--- src/stats.rs
+++ src/stats.rs
@@ -1,25 +1,4 @@
-use yew::prelude::*;
-use gloo_console::log;
-
-//use testbench_util::orion::orion::Stats;
-
-
 #[derive(serde::Deserialize, Clone, Copy, Debug, Default, PartialEq)]
 pub struct Stats {
     pub temperatures: [f32; 11],
-}
-
-
-#[derive(Properties, Clone, Copy, Debug, PartialEq)]
-pub struct Props {
-    pub stats: Stats,
-}
-
-
-#[function_component(StatsDisplay)]
-pub fn stats_display(Props { stats }: &Props) -> Html {
-    log!("StatsDisplay");
-    html! {
-        <p>{format!("{:#?}", *stats)}</p>
-    }
 }
